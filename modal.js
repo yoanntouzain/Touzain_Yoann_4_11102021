bec2677d171b52cc5aa94ff44995bda64cd49ac6
yoanntouzain <ytouzain@gmail.com>
2021-11-08 12:35:56 +0100

    finission des validation des formulaires "finission des validation des formulaires + mise au propre du js"

--- modal.js
+++ modal.js
@@ -19,7 +19,6 @@ const btnValid = document.querySelectorAll(".btn-validate");
 
 //variable champs
 const myForm = document.getElementById("myForm");
-/*
 const myName = document.getElementById("first");
 const mySecond = document.getElementById("last");
 const myEmail = document.getElementById("email");
@@ -31,8 +30,7 @@ const ville3 = document.getElementById("location3");
 const ville4 = document.getElementById("location4");
 const ville5 = document.getElementById("location5");
 const ville6 = document.getElementById("location6");
-const checkbox1 = document.getElementById("checkbox1"); 
-*/
+const checkbox1 = document.getElementById("checkbox1");
 
 
 //Constante erreur formulaire
@@ -44,13 +42,10 @@ const errorParticipation = document.getElementById("errorParticipation");
 const errorVilles = document.getElementById("errorVilles");
 const errorCondition = document.getElementById("errorCondition");
 
-//RegExp
-/*
-const myRegExp = /^[a-zA-Z-\s]{2,}$/;
-const emailRegExp = /[a-zA-Z0-9-]@[a-z.]/;
-const naissanceRegExp = /[0-9]/;
-const participationRegExp = /^[0-9]{1,}$/;
-*/
+//Mes RegExp
+const textRegExp = /^[a-zA-Z-\s]{2,}$/;
+const RegExpEmail = /[a-zA-Z0-9-]@[a-z.]/;
+const RegExpNumber = /[0-9]/;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -74,105 +69,35 @@ function btnGO() {
     contentMessage.style.display = "none";
 };
 
-
-/*
-
-PSEUDO CODE FORMULAIRE
-
-1- on définis comme variable chaque champs de formulaire (exemple: champs nom = varNomForm)
-
-
-2- créer un regex pour chaque type de formulaire
-
-3- Créer un tableau de type BOOLEAN 
-    3.1- relier chaque champs de formulaire à un indexe de tableau
-
-4- la page ne doit PAS s'actualiser
-
-5- on créer un message d'erreur unique pour chaque champs de formulaire (1 seul message d'erreur par champs)
-
-6- créer une fonction générale qui au moment du submit va utiliser des sous-fonction pour la vérifications des champs
-    6.1- cette fonction remplis un tableau de type BOOLEAN avec soit TRUE ou FALSE aprés varification des sous fonctions
-    6-2- les indexes du tableau sont relier a un champ de formulmaire unique (ex: champs prenom -> index 0, champs nom -> index 1 etc...)
-
-7- créer les sous fonctions de vérification
-    7.1- La fonction vérifie l'exactitude d'un formulaire par rapport à un REGEX définis au préalable
-    7.2- la fonction doit retourner TRUE ou FALSE a la fonction qui est étape 6
-
-8- créer une fonction d'appel de message d'erreur qui affiche un message d'erreur sous les champs incorecte OU vide
-    8.1- parcourt le tableau remplis et affiche le message d'erreur sous le(s) champs incorrectes ou vide
-    8.2- SI le formulaire est entierement TRUE ALORS afficher message de confirmation
-
-9- lancer l'etape de vérification au moment du submit (au moment du clique sur "c'est partie" lancer la fonction Etape 4)
-
-
-10 -etape finale: un message d'erreur s'affiche sous chaque champs de formulaire non remplis ou invalide OU message de confirmation si tout est ok
-
-*/
-
-//Mes variables
-    const myName = document.getElementById("first");
-    const mySecond = document.getElementById("last");
-    const myEmail = document.getElementById("email");
-    const myNaissance = document.getElementById("birthdate");
-    const myParticipation = document.getElementById("quantity");
-    const ville1 = document.getElementById("location1");
-    const ville2 = document.getElementById("location2");
-    const ville3 = document.getElementById("location3");
-    const ville4 = document.getElementById("location4");
-    const ville5 = document.getElementById("location5");
-    const ville6 = document.getElementById("location6");
-    const checkbox1 = document.getElementById("checkbox1");
-
-//Mes RegExp
-    const textRegExp = /^[a-zA-Z-\s]{2,}$/;
-    const RegExpEmail = /[a-zA-Z0-9-]@[a-z.]/;
-    const RegExpNumber = /[0-9]/;
-
 //Mon tableau
    const arrayBoolean = [];
 
-//Mes messages d'erreurs
-    const msgErrFirst = errorFirst.innerHtml = "Veuillez entrer 2 caractères ou plus pour le champ du Prénom.";
-    const msgErrSecond = errorSecond.innerHtml = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-    const msgErrEmail = errorEmail.innerHtml = "Veuillez devez entrer une adresse mail.";
-    const msgErrNaissance = errorNaissance.innerHtml = "Vous devez entrer votre date de naissance.";
-    const msgErrParticipation = errorParticipation.innerHtml = "Veuillez entrer une valeur numérique entière.";
-    const msgErrVilles = errorVilles.innerHtml = "Vous devez choisir une option.";
-    const msgErrCondition = errorCondition.innerHtml = "Vous devez vérifier que vous acceptez les termes et conditions.";
 //fonction submit
 document.forms["reserve"].addEventListener("submit", function(e) {
+    e.preventDefault();
+        
     prenom();
     arrayBoolean[0] = prenom(myName);
-    console.log(prenom(myName));
 
     nom();
     arrayBoolean[1] = nom(mySecond);
-    console.log(nom(mySecond));
 
     email();
     arrayBoolean[2] = email(myEmail);
-    console.log(email(myEmail));
 
     naissance();
     arrayBoolean[3] = naissance(myNaissance);
-    console.log(naissance(myNaissance));
 
     participation();
     arrayBoolean[4] = participation(myParticipation);
-    console.log(participation(myParticipation));
 
     ville();
     arrayBoolean[5] = ville();
-    console.log(ville());
 
     condition();
     arrayBoolean[6] = condition(checkbox1);
-    console.log(condition(checkbox1));
-    console.log(arrayBoolean);
 
     scrollArray(arrayBoolean);
-    e.preventDefault();
 });
 
 
@@ -227,6 +152,7 @@ document.forms["reserve"].addEventListener("submit", function(e) {
 
 function scrollArray(arrayBoolean) {
     if (arrayBoolean[0] && arrayBoolean[1] && arrayBoolean[2] && arrayBoolean[3] && arrayBoolean[4] && arrayBoolean[5] && arrayBoolean[6] == true) {
+        noneMessage();
         content.style.display = "none";
         contentMessage.style.display = "block";
         return alert("Le formulaire est envoyé!")
@@ -241,193 +167,68 @@ function scrollArray(arrayBoolean) {
     };
 };
 
+function noneMessage() {
+    errorFirst.style.display = "none";
+    errorSecond.style.display = "none";
+    errorEmail.style.display = "none";
+    errorNaissance.style.display = "none";
+    errorParticipation.style.display = "none";
+    errorVilles.style.display = "none";
+    errorCondition.style.display = "none";
+}
+
 function messagePrenom() {
         if (arrayBoolean[0] == true) {
+            errorFirst.style.display = "none";
         }else {
-            msgErrFirst
-            errorFirst.style.background = 'red'
-            console.log("non " + arrayBoolean[0])
+            errorFirst.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du Prénom.";
         }
 };
 
 function messageNom() {
         if (arrayBoolean[1] == true) {
+            errorSecond.style.display = "none";
         }else {
-            msgErrSecond
-            errorSecond.style.background = 'red'
-            console.log("non " + arrayBoolean[1])
+            errorSecond.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
         }
 };
 
 function messageEmail() {
         if (arrayBoolean[2] == true) {
+            errorEmail.style.display = "none";
         }else {
-            msgErrEmail
-            errorEmail.style.background = 'red'
-            console.log("non " + arrayBoolean[2])
+            errorEmail.innerHTML = "Veuillez devez entrer une adresse mail.";
         }
 };
 
 function messageNaissance() {
         if (arrayBoolean[3] == true) {
+            errorNaissance.style.display = "none";
         }else {
-            msgErrNaissance
-            errorNaissance.style.background = 'red'
-            console.log("non " + arrayBoolean[3])
+            errorNaissance.innerHTML = "Vous devez entrer votre date de naissance.";
         }
 };
 
 function messageParticipation() {
         if (arrayBoolean[4] == true) {
+            errorParticipation.style.display = "none";
         }else {
-            msgErrParticipation
-            errorParticipation.style.background = 'red'
-            console.log("non " + arrayBoolean[4])
+            errorParticipation.innerHTML = "Veuillez entrer une valeur numérique entière.";
         }
 };
 
 function messageVille() {
         if (arrayBoolean[5] == true) {
+            errorVilles.style.display = "none";
         }else {
-            msgErrVilles
-            errorVilles.style.background = 'red'
-            console.log("non " + arrayBoolean[5])
+            errorVilles.innerHTML = "Vous devez choisir une option.";
         }
 };
 
 function messageCondition() {
         if (arrayBoolean[6] == true) {
+            errorCondition.style.display = "none";
         }else {
-            msgErrCondition
-            errorCondition.style.background = 'red'
-            console.log("non " + arrayBoolean[6])
+            errorCondition.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
         }
 };
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-                                                                        // Fonction qui fonctionne
-/*
-function stopRefresh(e) {
-        e.preventDefault()
-        form()
-};
-btnSubmit.addEventListener("click", stopRefresh)
-
-//fonction pour vérifier la conformité du formulaire
-function form() {
-    prenom()
-    function prenom() {
-        if (myRegExp.test(myName.value) == false) {
-            errorFirst.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du Prénom.";
-            errorFirst.style.color = 'red';
-            alert("else if prenom false declencher");
-        }else if (myRegExp.test(myName.value) == true) {
-            errorFirst.innerHTML = "Le champs est correct";
-            errorFirst.style.color = 'green';
-            alert("else if prenom true declencher");
-            nomDeFamille()
-            console.log('prenom validé')
-        };
-    };
-    //vérication du nom de famille
-    function nomDeFamille() {
-        if (myRegExp.test(mySecond.value) == false) {
-            errorSecond.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du Prénom.";
-            errorSecond.style.color = 'red';
-            alert("else if false nom de famille");
-        }else if (myRegExp.test(mySecond.value) == true) {
-            errorSecond.innerHTML = "Le champs est correct";
-            errorSecond.style.color = 'green';
-            alert("c'est le 3eme true nom de famille");
-            Email()
-            console.log('nom de famille validé')
-        }
-    };
-    //vérication de l'email
-    function Email() {
-        if (emailRegExp.test(myEmail.value) == false) {
-            errorEmail.innerHTML = "Le champs est incorrect , veuillez entrer une adresse email valide. Exemple: gameon@gmail.com";
-            errorEmail.style.color = 'red';
-            alert("else if false email");
-        }else if (emailRegExp.test(myEmail.value) == true) {
-            errorEmail.innerHTML = "Le champs est correct";
-            errorEmail.style.color = 'green';
-            alert("c'est le 3eme true Email");
-            Naissance()
-            console.log('email validé')
-        }
-    };
-    //vérication de la naissance
-    function Naissance() {
-        if (naissanceRegExp.test(myNaissance.value) == false) {
-            errorNaissance.innerHTML = "Vous devez entrer une date de naissance correct";
-            errorNaissance.style.color = 'red';
-            alert("if false Naissance");
-        }else if (naissanceRegExp.test(myNaissance.value) == true) {
-            errorNaissance.innerHTML = "Le champs est correct";
-            errorNaissance.style.color = 'green';
-            alert("c'est le 3eme true Naissance");
-            Participation()
-            console.log('naissance validé')
-        }
-    };
-    //vérication de la participation
-    function Participation() {
-        if (participationRegExp.test(myParticipation.value) == false) {
-            errorParticipation.innerHTML = "Vous devez entrer un nombre entier";
-            errorParticipation.style.color = 'red';
-            alert("if false Participation");
-        }else if (participationRegExp.test(myParticipation.value) == true) {
-            errorParticipation.innerHTML = "Le champs est correct";
-            errorParticipation.style.color = 'green';
-            alert("c'est le 3eme true Participation");
-            villes()
-            console.log('Participation validé')
-        }
-    };
-    //vérication de la ville
-    function villes() {
-        if(ville1.checked || ville2.checked || ville3.checked || ville4.checked || ville5.checked || ville6.checked) {
-            errorVilles.innerHTML = "Le champs est correct";
-            errorVilles.style.color = 'green';
-            condition()
-            console.log('ville est correct')
-        }else{
-            errorVilles.innerHTML = "Vous devez cochez une ville";
-            errorVilles.style.color = 'red';
-            alert("else villes ");
-            console.log('probleme ville incorrect');
-        }
-    };
-    //vérication de la condition d'utilisation
-    function condition() {
-        if(checkbox1.checked) {
-            errorCondition.innerHTML = "Le champs est correct";
-            errorCondition.style.color = 'green';
-            content.style.display = "none";
-            contentMessage.style.display = "block";
-            console.log('Tout est bon')
-        }else{
-            errorCondition.innerHTML = "Vous devez accepter les conditions d'utilisations";
-            errorCondition.style.color = 'red';
-            alert("else condition ");
-            console.log('probleme else condition')
-        }
-    };
-};*/
